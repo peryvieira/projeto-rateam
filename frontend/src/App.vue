a62c1e36c687a2d3574f4e87f30c9e75b6e1be16
Pery Vieira <peryvieira_@hotmail.com>
2020-10-12 03:40:54 -0300

    feat: criacao view login

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,49 @@
 <template>
-  <nav-bar></nav-bar>
+  <v-app class="nav">
+    <v-app-bar
+            app
+            color="yellow"
+            dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+                alt="Rate Team"
+                class="shrink mr-2"
+                contain
+                src="@/assets/logoRate.png"
+                transition="scale-transition"
+                width="40"
+        />
+
+        <div style="color:orange" class="shrink mt-1 hidden-sm-and-down"
+        >Rate Team </div>
+      </div>
+    </v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <component :is="views"></component>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <div>footer</div>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 
-import NavBar from './components/common/NavBar'
+  import Login from "./view/Login/Login";
 export default {
   name: 'App',
-
-  components: {
-   NavBar
+  computed:{
+    views(){
+      return 'login'
+    }
   },
 
-  data: () => ({
-    //
-  }),
+  components: {
+    login: Login
+  },
 };
 </script>
---
+++ frontend/src/view/Login/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-layout>
+        <v-col offset="3" align-self="center">
+            <v-card class="pa-3" max-width="500px">
+                <v-card-title>Login</v-card-title>
+                <v-text-field label="usuario" dense outlined></v-text-field>
+                <v-text-field label="senha" dense type="password" outlined></v-text-field>
+                <v-card-actions class="justify-center">
+
+                    <v-btn>Cadastrar</v-btn>
+                    <v-btn>Entrar</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-col>
+    </v-layout>
+</template>
+
+<script>
+export default {
+    name:'Login',
+    components:{
+    }
+}
+</script>
+
+<style>
+</style>
